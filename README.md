c4f42ce3840b85cc3478338f819bb935b5139ac1
Roopak A N <anroopak@gmail.com>
2016-12-19 08:47:43 +0530

    Readme added. | Socket test cases uncommented.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-https://www.npmjs.com/package/pubsub-js
-https://github.com/rajaraodv/redispubsub
+# Post Scrabble - A Game of Words!!!
+
+## Rules
+1. This is 2-4 player game. Any person can start a game. Others can join the game. 
+2. Every player is assigned 7 tiles. Each tile is a letter, with a pre-defined **point**.
+3. On their turn, each player is supposed to create a meaningful name using their tiles, and one of the tiles from last word. 
+4. Points assigned to each tile is added to a player, when the tile is used. 
+
+## Missing Items
+1. End Game - a trigger to end game. 
+2. Pass Turn feature
+3. Moderated Game rooms - Owner can moderate the users. 
+
+## TODO:
+1. Use DB to store the User, Gameroom details (now, in-memory)
+2. Complete the Unit test cases. 
--- tests/test-socket.js
+++ tests/test-socket.js
@@ -34,52 +34,52 @@ describe('Room', function() {
 			}
 		});
 	});
-	// it('Join Room | True', function(done) {
-	// 	socket1.emit('join', {
-	// 		name: 'abcd',
-	// 		user: {
-	// 			name: "Deepak"
-	// 		}
-	// 	});
-	// 	socket1.on('join', data => {
-	// 		data.status.should.be.true();
-	// 		done();
-	// 	});
-	// });
-	// it('Join Room | Room does not exist', function(done) {
-	// 	socket2.emit('join', {
-	// 		name: 'abcde',
-	// 		user: {
-	// 			name: "Deepak"
-	// 		}
-	// 	});
-	// 	socket2.on('join', data => {
-	// 		data.status.should.be.false();
-	// 		done();
-	// 	});
-	// });
-	// it('Join Room | Exceeds Limit', function(done) {
-	// 	socket3.emit('join', {
-	// 		name: 'abcd',
-	// 		user: {
-	// 			name: "User 3"
-	// 		}
-	// 	});
-	// 	socket4.emit('join', {
-	// 		name: 'abcd',
-	// 		user: {
-	// 			name: "User 4"
-	// 		}
-	// 	});
-	// 	socket5.emit('join', {
-	// 		name: 'abcd',
-	// 		user: {
-	// 			name: "User 5"
-	// 		}
-	// 	});
-	// 	socket5.on('join', data => {
-	// 		data.message.should.equal("ROOM_MEM_COUNT_EXCEED");
-	// 		done();
-	// 	});
-	// });
+	it('Join Room | True', function(done) {
+		socket1.emit('join', {
+			name: 'abcd',
+			user: {
+				name: "Deepak"
+			}
+		});
+		socket1.on('join', data => {
+			data.status.should.be.true();
+			done();
+		});
+	});
+	it('Join Room | Room does not exist', function(done) {
+		socket2.emit('join', {
+			name: 'abcde',
+			user: {
+				name: "Deepak"
+			}
+		});
+		socket2.on('join', data => {
+			data.status.should.be.false();
+			done();
+		});
+	});
+	it('Join Room | Exceeds Limit', function(done) {
+		socket3.emit('join', {
+			name: 'abcd',
+			user: {
+				name: "User 3"
+			}
+		});
+		socket4.emit('join', {
+			name: 'abcd',
+			user: {
+				name: "User 4"
+			}
+		});
+		socket5.emit('join', {
+			name: 'abcd',
+			user: {
+				name: "User 5"
+			}
+		});
+		socket5.on('join', data => {
+			data.message.should.equal("ROOM_MEM_COUNT_EXCEED");
+			done();
+		});
+	});
 });
